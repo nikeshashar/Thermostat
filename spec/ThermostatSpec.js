73d5ecc91763b198325d42907d479880a90ca738
nikesh <nikeshashar@gmail.com>
2014-09-20 11:00:13 +0100

    Added increase temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,11 @@ describe("Thermostat", function() {
   it("should begin at 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  describe('increasing the temperature', function() {
+    it('increases if < 25 degrees', function() {
+      thermostat.increaseTemperature()
+      expect(thermostat.temperature).toEqual(21);
+    })
+  })
 });
